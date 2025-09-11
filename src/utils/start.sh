372b26686097b43aa8f66eaec3c3a730764a8897
Sergey Nezbritskiy <sergey.nezbritskiy@gmail.com>
2025-09-11 22:28:22 +0200

    enable xdebug with docker compose

--- src/utils/start.sh
+++ src/utils/start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+if [[ ${PHP_XDEBUG} -eq "1" ]]; then
+   xdebug on
+else
+  xdebug off
+fi
+
 sudo /usr/sbin/postfix start
 sudo /usr/local/sbin/php-fpm -F -c /usr/local/etc/php/php.ini
